86ea9a6d676ac500a7c60c5a018e41f3cc0bc4ed
Izarothy <izarothmail@gmail.com>
2022-08-07 21:05:24 +0200

    Hook TypeInput with the store

--- src/components/TypeInput.tsx
+++ src/components/TypeInput.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from '../lib/hooks';
+import { setTypeInput } from '../redux/slices/typeInputSlice';
 
 const TypeInput = () => {
-  return <input type="text" className="fixed bg-secondary-dark" />;
+  const dispatch = useDispatch();
+
+  const handleTypeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setTypeInput(e.target.value));
+  };
+
+  const input = useAppSelector((state) => state.typeInput.value);
+
+  return (
+    <input type="text" className="fixed bg-secondary-dark" onChange={(e) => handleTypeInput(e)} />
+  );
 };
 
 export default TypeInput;
