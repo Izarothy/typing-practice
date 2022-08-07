071e00815c66f8ca88e2574612f1839fdff3989d
Izarothy <izarothmail@gmail.com>
2022-08-07 20:43:06 +0200

    Set up colors and remove index.tsx boilerplate

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-dark': '#040405',
+        'secondary-dark': '#0c0d10',
+        'primary-blue': '#3a71c1',
+      },
+      backgroundImage: {
+        main: 'radial-gradient(circle at top center, #14151b, #0c0d10)',
+      },
+    },
   },
   plugins: [],
 };
