630d40a4da90a0b80a2a6cf83cf7d596e1fb567a
Izarothy <izarothmail@gmail.com>
2022-08-10 16:29:24 +0200

    Check for inputText and practiceText matching

--- src/components/TypeInput.tsx
+++ src/components/TypeInput.tsx
@@ -1,21 +1,27 @@
 import React, { ChangeEvent } from 'react';
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from '../lib/hooks';
+import { incrementCurrentTextIndex } from '../redux/slices/currentTextIndexSlice';
 import { setTypeInput } from '../redux/slices/typeInputSlice';
 
 const TypeInput = () => {
   const dispatch = useDispatch();
 
+  const practiceText = useAppSelector((state) => state.practiceText.value);
+  const currentTextIndex = useAppSelector((state) => state.currentTextIndex.value);
+
   const handleTypeInput = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch(setTypeInput(e.target.value));
-  };
 
-  const input = useAppSelector((state) => state.typeInput.value);
+    if (e.target.value[currentTextIndex] === practiceText[currentTextIndex]) console.log('');
+    dispatch(incrementCurrentTextIndex(1));
+  };
 
   return (
     <input
       type="text"
-      className="absolute left-8 bg-white/0 text-white/0 focus:outline-none"
+      autoFocus
+      className="absolute left-8 text-white/0 focus:outline-none"
       onChange={(e) => handleTypeInput(e)}
     />
   );
