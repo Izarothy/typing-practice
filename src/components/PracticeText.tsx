d8bc7b70c30273e9eba1868343d0c97c6abe0428
Izarothy <izarothmail@gmail.com>
2022-08-10 16:48:20 +0200

    Display text as separate HTML element letters

--- src/components/PracticeText.tsx
+++ src/components/PracticeText.tsx
@@ -3,7 +3,15 @@ import { useAppSelector } from '../lib/hooks';
 
 const PracticeText = () => {
   const practiceText = useAppSelector((state) => state.practiceText.value);
-  return <div>{practiceText}</div>;
+  return (
+    <div className="inline-flex gap-[3px]">
+      {practiceText.split('').map((letter, idx) => (
+        <span key={idx} id={`L${idx.toString()}`}>
+          {letter}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default PracticeText;
