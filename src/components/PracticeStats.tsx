263ea788a85016e3b0186ec690a12dcd1c3e5978
Izarothy <izarothmail@gmail.com>
2022-08-11 15:15:29 +0200

    Display practiceStats

--- src/components/PracticeStats.tsx
+++ src/components/PracticeStats.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import getPracticeStats from '../lib/getPracticeStats';
+import { useAppSelector } from '../lib/hooks';
 
 const PracticeStats = () => {
-  return <div>CPM: </div>;
+  const { startTime, endTime } = useAppSelector((state) => state.practiceTime.value);
+  const practiceText = useAppSelector((state) => state.practiceText.value);
+
+  const characterCount = practiceText.length;
+
+  const { CPM, secondsElapsed } = getPracticeStats(characterCount, startTime, endTime);
+  return (
+    <div>
+      CPM: {CPM} <br /> Seconds elapsed: {secondsElapsed}
+    </div>
+  );
 };
 
 export default PracticeStats;
